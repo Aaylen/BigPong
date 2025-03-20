98656dc5108f67dac14377b5511bf3b1d734c3da
Alexander Aylen <tubbyaylen@gmail.com>
2025-03-20 13:43:28 -0700

    updated with IP

--- player.py
+++ player.py
@@ -2,6 +2,6 @@ import socket
 import os
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("", 6000))
+client.connect(("10.16.8.220", 6000))
 client.send("Hello from the client".encode())
 print("Connected to the server")
