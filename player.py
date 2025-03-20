0d39459713fe10ea3385e184678a473a1d2e31f8
Alexander Aylen <tubbyaylen@gmail.com>
2025-03-20 13:32:47 -0700

    some changes

--- player.py
+++ player.py
@@ -1,9 +1,7 @@
 import socket
 import os
 
-masterIP = os.environ.get('masterIP')
-
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("10.16.9.13", 6000))
+client.connect(("", 6000))
 client.send("Hello from the client".encode())
 print("Connected to the server")
